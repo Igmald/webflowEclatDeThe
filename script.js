eaad4af777b7951f178c98d8029c98cb331440d0
Igmald <119805481+Igmald@users.noreply.github.com>
2026-01-18 11:44:35 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 import * as THREE from "three";
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader.js';
+
+
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { RGBELoader } from 'three/addons/loaders/RGBELoader.js';
 import GUI from 'lil-gui';
+
 
 // GUI
 const gui = new GUI();
